ac402a0bc9870bb6219dc2482666c86e37c4d0ab
FakeRanjanoob <Ranjan Parida>
2025-10-05 11:21:35 +0530

    Feat: Created the fetch weather logic using async function

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ const temperatureEl = document.getElementById('temperature');
 const conditionTextEl = document.getElementById('condition-text');
 const conditionIconEl = document.getElementById('condition-icon');
 const errorMessageEl = document.getElementById('error-message');
+
+
+
+const api_key = "9629e2e3bbfa73edd343b9bae3f60a26";
+
+
+async function fetchWeather(cityName){
+    try{
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api_key}`);
+         if (response.status === 400) {
+            throw new Error('City not found. Please try again.');
+        }
+        if (!response.ok) {
+            throw new Error('An error occurred. Please try again later.');
+        }
+
+        const data = await response.json();
+        updateUI(data);
+
+    }
+
+}
